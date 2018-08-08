ab8a16d4c13eae6e493e7e34b8d5ee8953b7672f
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-08 10:15:51 -0400

    made changes to layout and css file, added social buttons, added skills

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+   console.log("DOM fully loaded and parsed");
+
+var heading = document.querySelector("#iam");
+                var data = ["Hi! I'm Bibek 😄","I am a web developer!","I ❤️ solving problems!", "I love to travel"];
+                var j = 0;
+                var i = 0;
+
+
+            var k;
+
+            function typing(){
+                if(heading.innerHTML.length === data[j].length){
+                    k = 0;
+                    setTimeout(del, 1200);
+                }
+                if(i<data[j].length){
+                    heading.innerHTML += data[j].charAt(i);
+                    i++;
+                    setTimeout(typing, 50);
+                }
+                else{
+                    j++;
+                    i = 0;
+                    k = 0;
+                    setTimeout(typing, 3000);
+                }
+                if(j === data.length){
+                    j = 0;
+                }
+            }
+
+            // var check = document.getElementById("check");
+            // var k = 0;
+            function del(){
+                if(heading.innerHTML.length == 0){
+                    clearTimeout(time);
+                }
+                else{
+                heading.innerHTML = heading.innerHTML.slice(0, heading.innerHTML.length - k);
+                k++;
+                var time = setTimeout(del, 100);
+                }
+            }
+
+            typing();
+
+          // hamburger menu event addEventListener
+
+          var hamburger = document.querySelector('.hamburger');
+          var mainnav = document.querySelector('.main-nav');
+
+
+          hamburger.addEventListener('click', function(event){
+            event.preventDefault()
+            if (mainnav.style.display === "none") {
+              mainnav.style.display = "block";
+            } else {
+            mainnav.style.display = "none";
+            }
+          })
+
+});
