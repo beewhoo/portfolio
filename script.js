32ab0704643fa9d815866420e1686f56ce0c3cf0
beewhoo <bibeksinghtthapa@gmail.com>
2018-08-10 18:14:17 -0400

    replace the old hamburger menu to new one

--- script.js
+++ script.js
@@ -47,17 +47,29 @@ var heading = document.querySelector("#iam");
 
           // hamburger menu event addEventListener
 
-          var hamburger = document.querySelector('.hamburger');
-          var mainnav = document.querySelector('.main-nav');
 
+var menu = document.querySelector('.menu');
+var menuList = document.querySelector('.menu-items');
 
-          hamburger.addEventListener('click', function(event){
-            event.preventDefault()
-            if (mainnav.style.display === "none") {
-              mainnav.style.display = "block";
-            } else {
-            mainnav.style.display = "none";
-            }
-          })
+var menuOpened = false;
+
+//hover over the menu icon -- opens the mini menu
+menu.addEventListener('mouseover', function(){
+  menuList.style.left = '0';
+});
+//meave the menu-icon hides mini-icons
+menu.addEventListener('mouseleave', function(){
+  menuList.style.left = '-81px';
+});
+
+menuList.addEventListener('mouseover', function(){
+  menuList.style.width = '300px';
+  menuList.style.left = '0';
+});
+
+menuList.addEventListener('mouseleave', function(){
+  menuList.style.width = '80px';
+  menuList.style.left = '-81px';
+});
 
 });
